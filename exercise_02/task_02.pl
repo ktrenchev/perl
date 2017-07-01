05c83fbf0201d416b3a735eb4cc2940387561a8e
kpacu <k.trenchev@kmail.bg>
2017-07-01 14:04:02 +0300

    exercise_02/task_02.pl: Reads a circle's radius and prints it's circumference.
    Reads a circle's radius from STDIN, computes the circumference of the said
    circle and prints it to STDOUT. Output is a bit more verbose than required.

---
+++ exercise_02/task_02.pl
@@ -0,0 +1,12 @@
+#!/usr/bin/perl
+
+sub find_circumference {
+	my $radius = shift;
+	my $pi = 3.1415926535897932384626433832; # That is all the digits I know :/
+	return 2 * $radius * $pi;
+}
+
+my $radius = <STDIN>;
+chomp($radius);
+print "The circumference of a circle with a radius $radius is ", find_circumference($radius), "\n";
+exit 0;
