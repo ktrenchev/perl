8af1df03739e4668b5426d39455e58bdf2dc65aa
kpacu <k.trenchev@kmail.bg>
2017-07-01 14:33:44 +0300

    exercise_02/task_04.pl: Added prompting for numbers.

--- exercise_02/task_04.pl
+++ exercise_02/task_04.pl
@@ -1,7 +1,8 @@
 #!/usr/bin/perl
 
-
+print "Please enter the first number:\n";
 chomp (my $first = <STDIN>);
+print "Please enter the second number:\n";
 chomp (my $second = <STDIN>);
 
 print $first * $second, "\n";
